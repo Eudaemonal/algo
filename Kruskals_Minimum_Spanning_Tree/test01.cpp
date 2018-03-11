3aa15404f3ca30a3ba3cdb77e77c77041ed5e19f
eudaemonal <eudaemonal@gmail.com>
2018-03-11 17:45:30 +1100

    finish

--- Kruskals_Minimum_Spanning_Tree/test01.cpp
+++ Kruskals_Minimum_Spanning_Tree/test01.cpp
@@ -12,50 +12,83 @@ struct Edge{
 
 class Graph{
 public:
-	Graph()=default;
+	Graph(int _n)
+		:n(_n)
+	{}
 
 	void add(int src, int dst, int weight){
 		Edge e;
 		e.src = src;
 		e.dst = dst;
 		e.weight = weight;
-		map.push_back(e);
+		connections.push_back(e);
 	}
 
-	void kruskalMST(){
-		int m = 0;
-		std::sort(map.begin(), map.end(), [](Edge &a, Edge &b){
+	std::vector<Edge> kruskalMST(){
+		std::vector<int> group(n);
+		std::vector<Edge> mst;
+		int size = 0;
+		std::sort(connections.begin(), connections.end(), [](Edge &a, Edge &b){
 			return a.weight < b.weight;
 		});
 		
-		for(int i = 0; i < map.size(); ++i){
-			if(map[i].group==-1){
-				map[i].group=;
-				
+		for(int i = 0; i < connections.size(); ++i){
+			if(group[connections[i].src]==0 && group[connections[i].dst]==0)
+			{
+				size++;
+				group[connections[i].src]=size;
+				group[connections[i].dst]=size;
+				mst.push_back(connections[i]);
 			}
-		}
+			else if(group[connections[i].src]==0 && group[connections[i].dst]!=0 )
+			{
+				group[connections[i].src]=group[connections[i].dst];
+				mst.push_back(connections[i]);
+			}
+			else if(group[connections[i].src]!=0 && group[connections[i].dst]==0 )
+			{
+				group[connections[i].dst]=group[connections[i].src];
+				mst.push_back(connections[i]);
 
+			}
+			else if(group[connections[i].src]!=group[connections[i].dst])
+			{
+				// store original value as vector group will change
+				int g_src = group[connections[i].src];
+				int g_dst = group[connections[i].dst];
+				for(int j = 0; j < n; ++j){
+					if(group[j]==g_src){
+						group[j]=g_dst;
+					}
+				}
+				mst.push_back(connections[i]);
+			}
 
-		for(int i = 0; i < map.size(); ++i){
-			std::cout << map[i].weight<< "\n";
 		}
+		return mst;
 	}
 
 	
 private:
-	std::vector<Edge> map;
+	std::vector<Edge> connections;
+	int n;
 };
 
 
 int main(int argc, char *argv[]){
-	int n;
-	std::cin >> n;
-	Graph g;
+	int n, nodes;
+	std::cin >> n >> nodes;
+	Graph g(nodes);
 	for(int i = 0; i < n; ++i){
 		int src, dst, weight;
 		std::cin >> weight >> src >> dst;
 		g.add(src, dst, weight);
 	}
-	g.kruskalMST();
+	std::vector<Edge> mst= g.kruskalMST();
+	std::cout << "MST is:\n";
+	for(int i = 0; i < mst.size(); ++i){
+		std::cout << mst[i].weight<< " "<<  mst[i].src<<" " <<mst[i].dst << "\n";
+	}
+
 	return 0;
 }
