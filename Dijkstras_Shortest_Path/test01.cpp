cf64d142383155e12dd60e9d444649d5fd6c7387
eudaemonal <eudaemonal@gmail.com>
2018-03-14 14:19:14 +1100

    done

--- Dijkstras_Shortest_Path/test01.cpp
+++ Dijkstras_Shortest_Path/test01.cpp
@@ -24,17 +24,52 @@ public:
 		connections.push_back(e);
 	}
 
-	std::vector<Edge> dijkstra(int start){
-		for(int i=0; i < connections.size(); ++i){
-			
-		}
 
+	std::vector<int> dijkstra(int start){
+		std::vector<int> dist(n, INT_MAX);
+		setds.insert(setds.begin(), std::make_pair(start, 0));
+		dist[start] = 0;
+		while(!setds.empty()){
+			std::sort(setds.begin(), setds.end(), [](auto &a, auto &b){
+				return a.second < b.second;
+			});
+
+			std::pair<int, int> tmp = *(setds.begin());
+			setds.erase(setds.begin());
+			std::cout <<"erase("<< tmp.first << " "<< tmp.second << ")\n";
+
+			int u = tmp.first;
+			for(int i=0; i < connections.size(); ++i){
+				if(connections[i].src==u){
+					int v = connections[i].dst;
+					int w = connections[i].weight;
+					if(dist[v] > dist[u] + w){
+						dist[v] = dist[u] + w;
+						setds.insert(setds.begin(), std::make_pair(v, dist[v] ));
+						std::cout <<"insert("<< v << " "<< dist[v] << ")\n";
+					}
+				}else if(connections[i].dst==u){
+					int v = connections[i].src;
+					int w = connections[i].weight;
+					if(dist[v] > dist[u] + w){
+						dist[v] = dist[u] + w;
+						setds.insert(setds.begin(), std::make_pair(v, dist[v]));
+						std::cout <<"insert("<< v << " "<< dist[v] << ")\n";
+
+					}
+				}
+
+			}
+		}
+		return dist;
 
 	}
+
 
 	
 private:
 	std::vector<Edge> connections;
+	std::vector<std::pair<int, int>> setds;
 	int n;
 };
 
@@ -48,10 +83,10 @@ int main(int argc, char *argv[]){
 		std::cin >> weight >> src >> dst;
 		g.add(src, dst, weight);
 	}
-	std::vector<Edge> mst= g.kruskalMST();
-	std::cout << "MST is:\n";
-	for(int i = 0; i < mst.size(); ++i){
-		std::cout << mst[i].weight<< " "<<  mst[i].src<<" " <<mst[i].dst << "\n";
+	std::vector<int> distances= g.dijkstra(0);
+	std::cout << "Shotest path is:\n";
+	for(int i = 0; i < distances.size(); ++i){
+		std::cout <<"Node: " << i << " Distance: " << distances[i] << "\n";
 	}
 
 	return 0;
