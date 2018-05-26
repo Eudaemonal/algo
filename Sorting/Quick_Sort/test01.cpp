75862edc10043d98f423d296a92b08dd534a4628
eudaemonal <eudaemonal@gmail.com>
2018-05-26 09:45:21 +0800

    update gitignore for cmake

---
+++ Sorting/Quick_Sort/test01.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> v){
+	for(auto it: v)
+		os << it << " ";
+	os << "\n";
+	return os;
+}
+
+int partition(std::vector<int> &v, int low, int high){
+	int pivot = v[high];
+	int i = low -1;
+	for(int j = low; j < high; ++j){
+		if(v[j] <= pivot){
+			++i;
+			std::swap(v[i], v[j]);
+		}
+	}
+	std::swap(v[i+1], v[high]);
+	return i+1;
+}
+
+void quick_sort(std::vector<int> &v, int low, int high){
+	if(low >= high)
+		return;
+	int p = partition(v, low, high);
+	quick_sort(v, low, p-1);
+	quick_sort(v, p+1, high);
+}
+
+int main(int argc, char *argv[]){
+	int n;
+	std::cin >> n;
+	std::vector<int> v(n);
+	for(int i=0; i < n; ++i){
+		std::cin >> v[i];
+	}
+
+	quick_sort(v, 0, v.size()-1);
+	std::cout << v;
+	return 0;
+}
+
