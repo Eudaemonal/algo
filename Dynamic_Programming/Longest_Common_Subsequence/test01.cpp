adcc00567ac60ebae666b16c9f52fe25b51330e8
eudaemonal <eudaemonal@gmail.com>
2018-05-26 15:14:13 +0800

    update lcs

--- Dynamic_Programming/Longest_Common_Subsequence/test01.cpp
+++ Dynamic_Programming/Longest_Common_Subsequence/test01.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-int lcs(std::string s1, std::string s2){
+int lcs(const std::string s1, const std::string s2){
+	int n = s1.length();
+	int m = s2.length();
+
+	std::vector<std::vector<int>> dp(n+1, std::vector<int>(m+1));
+	for(int i=1; i < n+1; ++i){
+		for(int j= 1; j < m+1; ++j){
+			if(s1[i-1]==s2[j-1]){
+				dp[i][j] = dp[i-1][j-1] + 1;
+			}else{
+				dp[i][j] = std::max(dp[i][j-1], dp[i-1][j]);
+			}
+		}
+	}
+	return dp[n][m];
 }
 
 int main(int argc, char *argv[]){
