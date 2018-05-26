44b0fdab84bbba5ac5abdd9bad0fc41fbfdca5aa
eudaemonal <eudaemonal@gmail.com>
2018-05-26 14:42:11 +0800

    update

---
+++ Dynamic_Programming/Longest_Common_Subsequence/test01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T> &v){
+	for(size_t i=0; i < v.size(); ++i){
+			os << v[i] << " ";
+	}
+	os << "\n";
+	return os;
+}
+
+int f(std::vector<int> v){
+	int n = v.size();
+	for(int i = 0; i < n; ++i){
+		
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	int n;
+	std::cin >> n;
+	std::vector<int> v(n);
+	for(int i = 0; i < n; ++i){
+		std::cin >> v[i];
+	}
+	
+	std::cout << f(v) << "\n";
+	return 0;
+}
